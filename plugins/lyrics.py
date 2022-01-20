0b2d1c9aa29050227e4a7d5e1f3ee18d4b5f96e5
cokepokess <97738509+cokepokess@users.noreply.github.com>
2022-01-20 19:15:00 +0400

    Update lyrics.py

--- plugins/lyrics.py
+++ plugins/lyrics.py
@@ -31,7 +31,7 @@ async def lyrics(medusa:Medusa,msg: Message):
     lyrics_text = lyric.lyrics
 
     try:
-        await r_text.edit_text(f'__--**{lyric_title}**--__\n__{lyric_artist}\n__\n\n__{lyrics_text}__\n__Extracted by @MedusaMousikibot__')
+        await r_text.edit_text(f'__--**{lyric_title}**--__\n__{lyric_artist}\n__\n\n__{lyrics_text}__\n__Extracted by @cokepokesmusic_bot')
 
     except MessageTooLong:
         with open(f'downloads/{lyric_title}.txt','w') as f:
@@ -44,7 +44,7 @@ async def lyrics(medusa:Medusa,msg: Message):
         await msg.reply_document(
             document=f'downloads/{lyric_title}.txt',
             thumb='src/Medusa320px.png',
-            caption=f'\n__--{lyric_title}--__\n__{lyric_artist}__\n\n__Extracted by @MedusaMousikibot__'
+            caption=f'\n__--{lyric_title}--__\n__{lyric_artist}__\n\n__Extracted by @cokepokesmusic_bot'
         )
 
         await r_text.delete()
